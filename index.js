202acf6517ab2f412c964744dc567650985413ff
jiwen xin <kevinxin@scripps.edu>
2019-11-22 13:59:26 -0800

    add list_all_classes feature

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const loadJsonFile = require('load-json-file');
 
-exports.Parser = class Parser {
+exports.Parser = class {
     constructor() {
         this.path = './schemas/biothings/biothings_curie_kevin.jsonld';
     };
-    
+
     async load_json() {
         this.schema = await loadJsonFile(this.path);
         return this.schema;
+    }
+
+    async fetch_all_classes() {
+        if (typeof this.schema !== 'undefined') {
+            return this.schema["@graph"].map(function(item) {
+                if (item['@type'] == 'rdfs:Class'){
+                    return item['rdfs:label']
+                }
+            });
+        } else {
+            await this.load_json();
+            return this.fetch_all_classes();
+        }
     }
 }
   
