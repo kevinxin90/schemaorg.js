f9cbe9b49eb4fd30d548d1d0181e3dd65156532b
jiwen xin <kevinxin@scripps.edu>
2019-11-22 13:59:37 -0800

    add test cases for list_all_classes feature

--- test/parser.js
+++ test/parser.js
@@ -3,11 +3,42 @@ let ps = require('../index').Parser;
 const loadJsonFile = require('load-json-file');
 
 describe("BioThings Schema Parser", function() {
+    let parser = new ps();
+    beforeEach('load schema file', async function() {
+        let se = await parser.load_json();
+    });
     describe("Load schema file", function() {
-        it("schema file is loaded correctly", async function() {
-            let parser = new ps();
-            var hash = await parser.load_json();
+        it("schema file is loaded correctly", function() {            
             expect(parser.schema).to.be.a('Object');
+            expect(parser.schema).to.have.property('@graph');
+            expect(parser.schema).to.have.property('@context');
+            expect(parser.schema["@graph"]).to.be.a('Array');
+            expect(parser.schema["@graph"][0]).to.have.property('@id');
+        });
+    });
+    describe("List all calsses defined in schema", function() {
+        it("classes should include Gene, ChemicalSubstance, etc", async function() {
+            let all_classes = await parser.fetch_all_classes();
+            expect(all_classes).to.be.a('Array');
+            expect(all_classes).to.be.an('array').that.includes('Gene');
+            expect(all_classes).to.be.an('array').that.includes('ChemicalSubstance');
+            expect(all_classes).to.be.an('array').that.includes('DiseaseOrPhenotypicFeature');
+        });
+        it("test if schema has not been pre-loaded", async function() {
+            parser1 = new ps();
+            let all_classes = await parser1.fetch_all_classes();
+            expect(all_classes).to.be.a('Array');
+            expect(all_classes).to.be.an('array').that.includes('Gene');
+            expect(all_classes).to.be.an('array').that.includes('ChemicalSubstance');
+            expect(all_classes).to.be.an('array').that.includes('DiseaseOrPhenotypicFeature');
+        });
+        it("classes should not inclue properties or other non-classes", async function() {
+            let all_classes = await parser.fetch_all_classes();
+            expect(all_classes).to.be.an('array').that.does.not.include('gene');
+            expect(all_classes).to.be.an('array').that.does.not.include('bts:Gene');
+            expect(all_classes).to.be.an('array').that.does.not.include('entrez');
+            expect(all_classes).to.be.an('array').that.does.not.include('symbol');
+            expect(all_classes).to.be.an('array').that.does.not.include('metabolizedBy');
         });
     });
 })
